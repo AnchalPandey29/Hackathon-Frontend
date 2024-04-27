2f9bf1c064d2f28153e1d7c0d891e356058e430b
Rishabh Jaiswal <awesomerishabh18@gmail.com>
2024-04-28 02:26:48 +0530

    Done

--- src/App.js
+++ src/App.js
@@ -8,12 +8,8 @@ import NotFound from './components/NotFound';
 import Contact from './components/Contact';
 import Dashboard from './components/Dashboard';
 import EmpMgmt from './components/EmployeeManagement';
-<<<<<<< HEAD
-import UserLeave from './components/UserLeave'
-=======
 import HRleavelist from './components/HRleavelist';
 import HRLeavedetail from './components/HRLeavedetail';
->>>>>>> d957beb779268d830441ec446cb211da259a56cb
 
 const App = () => {
   return (
@@ -25,12 +21,8 @@ const App = () => {
         <Route element={<Contact/>} path="/contact" />  
         <Route element={<Dashboard/>} path="/dashboard" />
         <Route element={<EmpMgmt/>} path="/employee" />
-<<<<<<< HEAD
-        <Route element={<UserLeave/>} path="/userleave" />
-=======
         <Route element={<HRleavelist/>} path="/hrleavelist" />
         {/* <Route element={<HRLeavedetail/>} path="/hrleavedetail" /> */}
->>>>>>> d957beb779268d830441ec446cb211da259a56cb
 
 
         <Route path="*" element={<NotFound/>} />
